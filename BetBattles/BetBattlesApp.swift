6f3933480c90b326958d2ab0dcd96c95e4377466
edvinmujanovic <edvin.mujanovic@gmail.com>
2024-03-07 20:15:16 +0100

    Initial Commit

---
+++ BetBattles/BetBattlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetBattlesApp.swift
+//  BetBattles
+//
+//  Created by Edvin Mujanovic on 2024-03-07.
+//
+
+import SwiftUI
+
+@main
+struct BetBattlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
