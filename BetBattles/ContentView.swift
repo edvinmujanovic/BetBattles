cf7754df13490d15ae04674d9cb13b5cda93fd81
edvinmujanovicthule <edvin.mujanovic@gmail.com>
2024-10-12 02:35:13 +0200

    Removed button styling

--- BetBattles/ContentView.swift
+++ BetBattles/ContentView.swift
@@ -28,7 +28,6 @@ struct ContentView: View {
                             await loadMatchData()
                         }
                     }
-                    .buttonStyle(CustomButtonStyle())
                 }
             }
             .padding()
